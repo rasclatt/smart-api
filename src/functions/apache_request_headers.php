46f022b61d79f585ae131ab8a2f9996a1084ef8e
Rasclatt <rasclatt@me.com>
2021-08-17 11:12:25 -0700

    Made function smarter
    Builds out the cookie attribute and also adds in content-based attributes

--- src/functions/apache_request_headers.php
+++ src/functions/apache_request_headers.php
@@ -2,10 +2,35 @@
 function apache_request_headers()
 {
     foreach($_SERVER as $key => $value) {
-        if(stripos($key, 'http_') !== false) {
-            $k = str_replace(' ', '-', ucwords(str_replace('_', ' ', strtolower(substr($key, 5)))));
-            $array[$k] = $value;
+        $is_content = (stripos($key, 'content_') !== false);
+        $is_http = (stripos($key, 'http_') !== false);
+        
+        if(!$is_http && !$is_content)
+            continue;
+
+        $k = strtolower(trim(($is_http)? str_replace(' ', '-', ucwords(str_replace('_', ' ', strtolower(substr($key, 5))))) : str_replace('_', '-', strtolower($key))));
+        
+        if($k == 'cookie') {
+            $value = explode(';', urldecode($value));
+            $q = array_map(function($v) {
+                $arr = [];
+                parse_str($v, $arr);
+                
+                foreach($arr as $k => $val) {
+                    if(preg_match('/:|{|\[/', $val)) {
+                        $json = @json_decode($val, 1);
+                        $arr[$k] = (empty($json))? $val : $json;
+                    }
+                }
+                
+                return $arr;
+
+            }, $value);
+            
+            $array[$k] = $q;
         }
+        else
+            $array[$k] = $value;
     }
 
     return ($array)?? [];
