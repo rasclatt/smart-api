28328b678951330eda4bce17a613ee73970dab51
Rasclatt <rasclatt@me.com>
2021-08-18 01:27:28 -0700

    Update to only serve protected functions

--- src/Init.php
+++ src/Init.php
@@ -3,12 +3,20 @@ namespace SmartApi;
 
 abstract class Init
 {
+    protected $request;
     /**
      *	@description	
      *	@param	
      */
     public function listen(string $method, $request = null)
     {
-        return $this->{$method}($request);
+        $this->request = $request;
+
+        $Reflect = new \ReflectionObject($this);
+
+        if(!$Reflect->getMethod($method)->isProtected())
+            throw new \SmartApi\Exception('Serive is invalid.', 403);
+
+        return $this->{$method}($this->request);
     }
 }
