53cd758eb2d0e6411d7fd1be5cbf113d3890a525
Rasclatt <rasclatt@me.com>
2022-03-24 23:47:28 -0700

    Fix bug

--- src/Init.php
+++ src/Init.php
@@ -13,6 +13,8 @@ abstract class Init
         $this->request = $request;
 
         $Reflect = new \ReflectionObject($this);
+        # Fetch the method for it's access
+        $Method = $Reflect->getMethod($method);
         # Disallow protected and final types
         if(!$Method->isProtected() || $Method->isFinal())
             throw new \SmartApi\Exception('Service is not reachable.', 403);
