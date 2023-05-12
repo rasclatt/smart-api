fd799b5beef68b937a46b8e89a8d32ad23a0d9a6
Rasclatt <rasclatt@me.com>
2023-05-12 16:21:04 -0400

    Update Controller.php
    Allow caching by allowing string names for callables

--- src/Helpers/Controller.php
+++ src/Helpers/Controller.php
@@ -7,9 +7,14 @@ class Controller
 {
     /**
      *	@description	
-     *	@param	
+     *	@important  Note that caching is not possible when the controller takes an anonymous function for the $func param. 
      */
-    public static function add(string $obj, string $path, callable $func = null, string $dtoPath = null): void
+    public static function add(
+        string $obj,
+        string $path,
+        callable | string $func = null,
+        string $dtoPath = null
+    ): void
     {
         App::$controllers[] = [
             'object' => $obj,
@@ -26,12 +31,18 @@ class Controller
     {
         if(empty(App::$controllers))
             return null;
-
+        # Loop controllers
         foreach (App::$controllers as $filter) {
+            # Get the callback (string or func)
             $reg = $filter['path'];
-            //die("!^{$reg}$!");
+            # If there is a matching route, then run it
             if(preg_match("!^{$reg}$!", App::$dto->service, $match)) {
-                return $filter['callback']($match[0]);
+                $f = $filter['callback'];
+                # If there is a string or callback function, try and run it,
+                # pass in matched values from path
+                if(is_string($f) || is_callable($f)) {
+                    return $f($match[0]);
+                }
             }
         }
         return null;
